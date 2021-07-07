47ba5d3cf8c1c5abd83acb5948720da4308754ec
KellyTall <42193386+KellyTall@users.noreply.github.com>
2021-07-07 19:06:58 +1000

    new maps

---
+++ js/big_map_innersouth.js
@@ -0,0 +1,119 @@
+async function drawMap_topo() {
+
+
+    const SA2_map_innersouth = await d3.json("./../geo/SA2_simple.json")
+
+
+
+    SA2_map_innersouth.objects.SA2.geometries = SA2_map_innersouth.objects.SA2.geometries.filter(d => d.properties.SA4_NAME16 == "Sydney - City and Inner South")
+
+    // console.log(SA2_map)
+
+
+    SA2_topo_innersouth = topojson.feature(SA2_map_innersouth, SA2_map_innersouth.objects.SA2)
+
+    // console.log(SA2_topo)
+
+    const trains = await d3.csv("./../data/tfnsw_train.csv", function(d) {
+        return {
+            location_name: d.location_name,
+            latitude: +d.latitude,
+            longitude: +d.longitude
+
+        }
+    })
+
+    // console.log(trains)
+
+    let dimensions = {
+        width: window.innerWidth * .6,
+        margin: {
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10,
+        },
+    }
+
+
+
+
+
+    dimensions.boundedWidth = (dimensions.width - dimensions.margin.left - dimensions.margin.right)
+
+
+    const projection_SA2 = d3.geoEquirectangular()
+        .fitWidth(dimensions.boundedWidth, SA2_topo_innersouth)
+
+
+
+    const pathGenerator_SA2 = d3.geoPath(projection_SA2)
+
+
+    const [
+        [x0, y0],
+        [x1, y1]
+    ] = pathGenerator_SA2.bounds(SA2_topo_innersouth)
+
+
+    dimensions.boundedHeight = y1
+    dimensions.height = dimensions.boundedHeight + dimensions.margin.top + dimensions.margin.bottom
+
+
+
+    const wrapper = d3.select(".map_innersouth")
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height)
+
+    const bounds = wrapper
+        .append("g")
+        .style("transform", `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
+
+
+    const background = bounds
+        .selectAll(".background")
+        .data(SA2_topo_innersouth.features)
+        .join("path")
+        .attr("class", "SA2")
+        .attr("d", pathGenerator_SA2)
+
+
+    const labels = bounds
+        .selectAll("text")
+        .data(SA2_topo_innersouth.features)
+        .join("text")
+        .attr("class", "area_label")
+        .text(d => d.properties.SA2_NAME16)
+        .attr("x", d => pathGenerator_SA2.centroid(d)[0])
+        .attr("y", d => pathGenerator_SA2.centroid(d)[1])
+
+    const points = bounds
+        .selectAll("circle")    
+        .data(trains)    
+        .join("circle")
+        .attr("class", "circle")
+        .attr("cx", d => projection_SA2([d.longitude, d.latitude])[0])
+        .attr("cy", d => projection_SA2([d.longitude, d.latitude])[1])
+        .attr("r", 4)
+
+        const point_label = points
+        .select("text")    
+        .data(trains)    
+        .join("text")
+        .attr("class", "point_label")
+        .text(d => d.location_name)
+        .attr("x", d => projection_SA2([d.longitude+.001, d.latitude])[0])
+        .attr("y", d => projection_SA2([d.longitude + .002, d.latitude])[1])
+        
+    
+
+
+
+}
+
+drawMap_topo()
