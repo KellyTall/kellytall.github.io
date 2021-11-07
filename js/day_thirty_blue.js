70af004df2179c59786f512e3e446d415ea30f6f
KellyTall <42193386+KellyTall@users.noreply.github.com>
2021-11-07 18:51:46 +1100

    RGB

---
+++ js/day_thirty_blue.js
@@ -0,0 +1,105 @@
+async function drawMap_locations_blue() {
+
+
+
+    const AUS_map = await d3.json("./../geo/AUS_simple.json")
+    AUS_topo = topojson.feature(AUS_map, AUS_map.objects.AUS)
+
+    const blue = await d3.json("./../data/blue_names_OSM.geojson")
+
+
+    // console.log(AUS_topo)
+
+
+
+
+    const width = 500
+    const height = 500
+    const margin = { top: 40, right: 10, bottom: 10, left: 10 }
+    const radius = 4
+
+
+    const svg = d3.select(".blue_map")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .attr('transform', `translate(0,${margin.top})`)
+
+
+    const projection_AUS = d3.geoEquirectangular()
+        .fitWidth(width, AUS_topo)
+
+
+
+    const pathGenerator_AUS = d3.geoPath(projection_AUS)
+
+    const blues = await d3.csv("./../data/blue_names_OSM.csv", function(d) {
+        return {
+            lon: +d.lon,
+            lat: +d.lat
+
+        }
+    })
+
+
+    const blues_map = blues.map(d => {
+        const coords = projection_AUS([+d.lon, +d.lat])
+        return Object.assign({ x: coords[0], y: coords[1] }, d)
+    })
+
+
+    const hexgridReds = d3.hexgrid()
+        .extent([width, height])
+        .geography(AUS_topo)
+        .pathGenerator(pathGenerator_AUS)
+        .projection(projection_AUS)
+        .hexRadius(radius)
+
+    const hexReds = hexgridReds(blues_map)
+
+    colour = d3.scaleSequential(t => {
+        return d3.interpolateBlues(Math.pow(t, 20))
+    }).domain([...hexReds.grid.extentPointDensity].reverse())
+
+
+
+    // const AUS = svg
+    //     .append("g")
+    //     .selectAll("path")
+    //     .data(AUS_topo.features)
+    //     .join("path")
+    //     .attr("d", pathGenerator_AUS)
+    //     .attr("class", "AUS_RGB_map")
+
+
+    const hex = svg
+        .append('g')
+        .selectAll('.hex')
+        .data(hexReds.grid.layout)
+        .enter()
+        .append('path')
+        .attr('class', 'hex')
+        .attr('transform', d => `translate(${d.x} ${d.y})`)
+        .attr('d', hexReds.hexagon())
+        .style('fill', d => !d.pointDensity ? 'white' : colour(d.pointDensity))
+        .style('stroke', '#D9D9D9')
+        .style('stroke-width', 1)
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+drawMap_locations_blue()
+
+
---
+++ js/day_thirty_red.js
@@ -0,0 +1,102 @@
+async function drawMap_locations() {
+
+
+
+    const AUS_map = await d3.json("./../geo/AUS_simple.json")
+    AUS_topo = topojson.feature(AUS_map, AUS_map.objects.AUS)
+
+    const red = await d3.json("./../data/red_names_OSM.geojson")
+
+
+    // console.log(AUS_topo)
+
+
+
+
+    const width = 500
+    const height = 500
+    const margin = { top: 40, right: 10, bottom: 10, left: 10 }
+    const radius = 4
+
+
+    const svg = d3.select(".red_map")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .attr('transform', `translate(0,${margin.top})`)
+
+
+    const projection_AUS = d3.geoEquirectangular()
+        .fitWidth(width, AUS_topo)
+
+
+
+    const pathGenerator_AUS = d3.geoPath(projection_AUS)
+
+    const reds = await d3.csv("./../data/red_names_OSM.csv", function(d) {
+        return {
+            lon: +d.lon,
+            lat: +d.lat
+
+        }
+    })
+
+
+    const reds_map = reds.map(d => {
+        const coords = projection_AUS([+d.lon, +d.lat])
+        return Object.assign({ x: coords[0], y: coords[1] }, d)
+    })
+
+
+    const hexgridReds = d3.hexgrid()
+        .extent([width, height])
+        .geography(AUS_topo)
+        .pathGenerator(pathGenerator_AUS)
+        .projection(projection_AUS)
+        .hexRadius(radius)
+
+    const hexReds = hexgridReds(reds_map)
+
+    colour = d3.scaleSequential(t => {
+        return d3.interpolateReds(Math.pow(t, 20))
+    }).domain([...hexReds.grid.extentPointDensity].reverse())
+
+
+
+    // const AUS = svg
+    //     .append("g")
+    //     .selectAll("path")
+    //     .data(AUS_topo.features)
+    //     .join("path")
+    //     .attr("d", pathGenerator_AUS)
+    //     .attr("class", "AUS_RGB_map")
+
+
+    const hex = svg
+        .append('g')
+        .selectAll('.hex')
+        .data(hexReds.grid.layout)
+        .enter()
+        .append('path')
+        .attr('class', 'hex')
+        .attr('transform', d => `translate(${d.x} ${d.y})`)
+        .attr('d', hexReds.hexagon())
+        .style('fill', d => !d.pointDensity ? 'white' : colour(d.pointDensity))
+        .style('stroke', '#D9D9D9')
+        .style('stroke-width', 1)
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+drawMap_locations()
